0c0b0da3c5d70e16de5f01ccbadb70432f678a9c
Aashutosh <aashutoshsingh226@gmail.com>
2026-06-02 17:02:10 +0530

    added the accept function to accept connection

--- src/server.cpp
+++ src/server.cpp
@@ -2,14 +2,44 @@
 #include<unistd.h>
 #include<sys/socket.h>
 #include<netinet/in.h>
+#include<cstring>
+using namespace std;
 int main(){
-	int server_fd;   //what is server fd why such a name what is fd in it
-	server_fd=socket(AF_INET,SOCK_STREAM,0);
+	int server_fd=socket(AF_INET,SOCK_STREAM,0);
 	if(server_fd<0){
-	std::cout<<"socket creation failed \n";  //why is \n better than endl
-	return 1;
+		perror("socket connection failed");
+		return 1;
 	}
-	std::cout<<"socket created \n";
+	
+	cout<<"socket created \n";
+	sockaddr_in server_addr;
+	memset(&server_addr,0,sizeof(server_addr));
+
+	server_addr.sin_family=AF_INET;
+	server_addr.sin_addr.s_addr=INADDR_ANY;
+	server_addr.sin_port=htons(8080);
+	if(bind(server_fd, (sockaddr*)&server_addr, sizeof(server_addr))<0){
+		perror("Bind failed");
+		return 1;
+	}
+
+	cout<<"Bind sucessful to port 8080\n";
+
+	if(listen(server_fd,1200)<1){
+		perror("Listen fialed ");
+		return 1;
+	}
+	cout<<"Listening on 8080\n";
+	sockaddr_in client_address;
+	socklen_t client_len=sizeof(client_address);
+	int client_fd=accept(server_fd,(sockaddr*)&client_address,&client_len);
+	if(client_fd<0){
+		perror("Acceptance failed");
+		return 1;
+	}
+	cout<<"Client connected\n";
+	close(client_fd);
+
 	close (server_fd);
 	return 0;
 }
